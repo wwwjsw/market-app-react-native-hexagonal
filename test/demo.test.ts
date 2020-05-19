41e37127b20099a8bf76c63f5ccbc36897a28caf
Juan Abril <juandaabril@gmail.com>
2020-05-18 19:16:12 -0500

    add test support

---
+++ test/demo.test.ts
@@ -0,0 +1,13 @@
+describe('aaa', () => {
+    test('aaa', () => {
+        expect(true).toBeTruthy();
+    });
+
+    test('aaa2', () => {
+        expect(true).toBeTruthy();
+    });
+
+    test('aaa3', () => {
+        expect(true).toBeTruthy();
+    });
+});
